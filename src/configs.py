eb1a57ecb6ce4894580378764a32fa56bd1be09d
JoeyLau <csyiliu@comp.polyu.edu.hk>
2020-09-01 19:09:19 +0800

    Remove the parameter setting process from options.py to configs.py, For centralized setting

---
+++ src/configs.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/9/1 18:55
+# @Author  : LIU YI
+
+import numpy as np
+import pandas as pd
+import math
+import copy
+
+class Configs(object):
+
+    def __init__(self):
+
+        ## For FL training
+        self.data = 'mnist'
+        self.rounds = 100
+        self.frac = 1
+        self.user_num = 5
+        self.tau = 5
+        self.FL_LR = 0.01
+        self.model = 'cnn'
+        self.iid = 0
+        self.unequal = 1
+
+
+#
+#
+#
+#
+#
+#
+#         self.lamda = 500
+#
+#         self.his_len = 5
+#         self.info_num = 3
+#
+#
+#
+#         if self.data == 'cifar':
+#             theta_num = 62006
+#             data_size = np.array([40, 38, 32, 46, 44]) * 250 * 0.8
+#
+#         else:
+#             theta_num = 21840
+#             if self.user_num == 5:
+#                 data_size = np.array([10000, 12000, 14000, 8000, 16000]) * 0.8
+#             else:
+#                 data_size = pd.read_csv('Multi_client_data/'+str(self.user_num)+'mnist.csv')
+#                 data_size = np.array(data_size['data_size'].tolist())
+#
+#         self.D = (data_size / 10) * (32 * (theta_num + 10 * 28 * 28)) / 1e9
+#         self.alpha = 0.1
+#         self.tau = 1
+#         self.C = 20
+#         self.communication_time = np.random.uniform(low=10, high=20, size=self.user_num)
+#
+#         self.BATCH = 5   #todo
+#         self.A_UPDATE_STEPS = 5  # origin:5
+#         self.C_UPDATE_STEPS = 5
+#         self.HAVE_TRAIN = False
+#         self.A_LR = 0.00001  # origin:0.00003
+#         self.C_LR = 0.00001
+#         self.GAMMA = 0.95  # origin: 0.95
+#         self.dec = 0.3  #
+#
+#         self.EP_MAX = 1000  #todo
+#         self.EP_MAX_pre_train = 1000
+#
+#         self.EP_LEN = 100
+#
+#
+#         if self.user_num == 5:
+#             self.delta_max = np.array([1.4359949, 1.02592623, 1.54966248, 1.43532239, 1.4203678])
+#
+#             Loss = pd.read_csv('loss_mnist_500.csv')
+#             Loss = Loss.to_dict()
+#             Loss = Loss['1']
+#             loss_list = []
+#             for i in Loss:
+#                 loss_list.append(Loss[i])
+#
+#
+#             self.loss_list = copy.copy(loss_list)
+#             num = len(loss_list)
+#             buffer = 0
+#             profit_increase = []
+#             for i in range(0, num):
+#                 loss_list[i] = -math.log(loss_list[i])
+#
+#             for one in loss_list:
+#                 profit_increase.append(one - buffer)
+#                 buffer = one
+#
+#             self.acc_increase_list = profit_increase
+#
+#
+#
+#         else:
+#             self.delta_max = np.random.uniform(low=1, high=2, size=self.user_num)
+#             data_info = pd.read_csv('Multi_client_data/' + str(self.user_num) + 'user_' + 'mnist' + '_1_0.005.csv')
+#             accuracy_list = data_info['loss'].tolist()
+#             num = len(accuracy_list)
+#
+#             self.loss_list = copy.copy(accuracy_list)
+#             for i in range(0, num):
+#                 accuracy_list[i] = -math.log(accuracy_list[i])
+#
+#             buffer = -math.log(3)
+#
+#             self.acc_increase_list = []
+#             for one in accuracy_list:
+#                 self.acc_increase_list.append(one - buffer)
+#                 buffer = one
+#
+#         self.amplifier_baseline = np.max(self.delta_max * self.tau * self.C * self.D * self.alpha)
+#         self.amplifier_hrl = np.sum(self.delta_max * self.tau * self.C * self.D * self.alpha)
+#         self.reducer_baseline = 1
+#         self.reducer_hrl = 100
+#
+#         reducer_pretrain_dict = {
+#             5: 1000,
+#             10: 500,
+#             20: 50,
+#             30: 50,
+#             40: 50,
+#             40: 50,
+#             50: 50
+#         }
+#         self.reducer_pretrain = reducer_pretrain_dict[self.user_num]
+#
+#
+# if __name__ == '__main__':
+#
+#     np.random.seed(2)
+#     configs = Configs('mnist', 50)
+#
+#     print(configs.delta_max)
+#     print(configs.D)
+#     print(configs.amplifier_hrl)
+#     print(configs.amplifier_baseline)
+#     print(configs.communication_time)
+#     print(configs.acc_increase_list)
+#     # print(configs.D)
--- src/options.py
+++ src/options.py
@@ -3,29 +3,31 @@
 # Python version: 3.6
 
 import argparse
-
+from src.configs import Configs
 
 def args_parser():
+
+    configs = Configs()
     parser = argparse.ArgumentParser()
 
     # federated arguments (Notation for the arguments followed from paper)
-    parser.add_argument('--epochs', type=int, default=10,
+    parser.add_argument('--epochs', type=int, default=configs.rounds,
                         help="number of rounds of training")
-    parser.add_argument('--num_users', type=int, default=5,
+    parser.add_argument('--num_users', type=int, default=configs.user_num,
                         help="number of users: K")
-    parser.add_argument('--frac', type=float, default=1,
+    parser.add_argument('--frac', type=float, default=configs.frac,
                         help='the fraction of clients: C')
-    parser.add_argument('--local_ep', type=int, default=5,
+    parser.add_argument('--local_ep', type=int, default=configs.tau,
                         help="the number of local epochs: E")
     parser.add_argument('--local_bs', type=int, default=10,
                         help="local batch size: B")
-    parser.add_argument('--lr', type=float, default=0.01,
+    parser.add_argument('--lr', type=float, default=configs.FL_LR,
                         help='learning rate')
     parser.add_argument('--momentum', type=float, default=0.5,
                         help='SGD momentum (default: 0.5)')
 
     # model arguments
-    parser.add_argument('--model', type=str, default='cnn', help='model name')
+    parser.add_argument('--model', type=str, default=configs.model, help='model name')
     parser.add_argument('--kernel_num', type=int, default=9,
                         help='number of each kind of kernel')
     parser.add_argument('--kernel_sizes', type=str, default='3,4,5',
@@ -43,7 +45,7 @@ def args_parser():
                         strided convolutions")
 
     # other arguments
-    parser.add_argument('--dataset', type=str, default='mnist', help="name \
+    parser.add_argument('--dataset', type=str, default=configs.data, help="name \
                         of dataset")
     parser.add_argument('--num_classes', type=int, default=10, help="number \
                         of classes")
@@ -51,9 +53,9 @@ def args_parser():
                         to a specific GPU ID. Default set to use CPU.")
     parser.add_argument('--optimizer', type=str, default='sgd', help="type \
                         of optimizer")
-    parser.add_argument('--iid', type=int, default=0,
+    parser.add_argument('--iid', type=int, default=configs.iid,
                         help='Default set to IID. Set to 0 for non-IID.')
-    parser.add_argument('--unequal', type=int, default=1,
+    parser.add_argument('--unequal', type=int, default=configs.unequal,
                         help='whether to use unequal data splits for  \
                         non-i.i.d setting (use 0 for equal splits)')
     parser.add_argument('--stopping_rounds', type=int, default=10,
